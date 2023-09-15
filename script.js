0d69c3da6c890b549e9b1ff4ce3ea118007571b5
Stathis <133227039+Stathis92@users.noreply.github.com>
2023-09-15 20:52:40 +0300

    Add initial operator functions

--- script.js
+++ script.js
@@ -5,18 +5,50 @@ const displ = document.getElementById("display");
 numberBtn.forEach((button) => {
   button.addEventListener("click", () => {
     //NUMBER BUTTON CLICKED FUNCTION
-    if (button.id.slice(3) == "Dec") {
-      displ.innerHTML += ".";
-    } else {
-      displ.innerHTML += parseInt(button.id.slice(3));
-    }
+    registerNumber(button.innerHTML);
   });
 });
 
 operatorBtn.forEach((button) => {
   button.addEventListener("click", () => {
+
     //OPERATOR BUTTON CLICKED FUNCTION
+    tempNumber = parseInt(displ.innerHTML);
+    tempOper = button.id;
+    displ.innerHTML += button.innerHTML;
+    operate(tempOper, tempNumber, tempNumber2);
   });
 });
 
-function operate(oper, num1, num2) {}
+function registerNumber(btn){
+    if (displ.innerHTML == 0)
+    {
+    displ.innerHTML = btn;
+    }else{
+    displ.innerHTML += btn;
+    }
+}
+
+function operate(oper, num1, num2) {
+
+}
+
+function add(num1, num2){
+    return parseInt(num1)+parseInt(num2);
+}
+
+function sub(num1, num2){
+    return num1-num2;
+}
+
+function mult(num1, num2){
+    return num1*num2;
+}
+
+function divide(num1, num2){
+    if(num2 == 0)
+     return "I AM ERROR :D";
+    return num1/num2;
+}
+
+let tempNumber = 0;
